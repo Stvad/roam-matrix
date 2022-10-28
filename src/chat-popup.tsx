ad85e37ce9c244b0d861f462806bc7dade6adb48
Vladyslav Sitalo <sitalov@amazon.com>
2022-10-28 10:20:25 -0700

    basic popup setup

---
+++ src/chat-popup.tsx
@@ -0,0 +1,32 @@
+import {Classes, Dialog} from '@blueprintjs/core'
+import React from 'react';
+import {createOverlayRender} from 'roamjs-components/util'
+import {useState} from 'react'
+
+
+interface ChatPopupProps {
+    pageId: string;
+}
+
+export const ChatPopup = ({onClose, pageId}: { onClose: () => void; } & ChatPopupProps) => {
+
+
+    return (
+        <Dialog
+            isOpen={true}
+            onClose={onClose}
+            canEscapeKeyClose
+            backdropClassName={'autocomplete-dialog-backdrop'}
+            className={'autocomplete-dialog'}
+        >
+            <div className={Classes.DIALOG_BODY + ' autocomplete-dialog-body'}>
+
+                <h3>Chat {pageId}</h3>
+
+            </div>
+        </Dialog>
+    )
+}
+
+// @ts-ignore
+export const RoomChatOverlay = createOverlayRender<ChatPopupProps>('autocomplete-prompt', ChatPopup)
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import runExtension from "roamjs-components/util/runExtension";
+import {RoomChatOverlay} from './chat-popup'
 
 export default runExtension({
   run: (args) => {
@@ -6,5 +7,11 @@ export default runExtension({
       tabTitle: "roam-matrix",
       settings: [],
     });
+
+    window.roamAlphaAPI.ui.commandPalette.addCommand({
+      label: "Page chat",
+      callback: async () => RoomChatOverlay({pageId: await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid()}),
+    })
+    console.log("roam-matrix loaded")
   },
 });
